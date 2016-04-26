c7ddebf74be57578e53ab033f7e23990be91d408
red3141 <red3141@gmail.com>
2016-04-26 00:06:36 -0400

    Basic bass drum

--- www/urf_tunes.js
+++ www/urf_tunes.js
@@ -12,6 +12,35 @@ function init() {
 function playFrequency(frequency) {
     var oscillator = context.createOscillator();
     oscillator.frequency.value = frequency;
+    var i = 0;
+    setInterval(function() { oscillator.frequency.value += 50 * Math.sin(i); i += 0.3;}, 100);
     oscillator.connect(context.destination);
     oscillator.start(0);
+}
+
+var bassDrumOscillator;
+var bassDrumGain;
+
+function playBassDrum() {
+    if (!bassDrumOscillator) {
+        bassDrumOscillator = context.createOscillator();
+        bassDrumGain = context.createGain();
+        bassDrumGain.gain.value = 0;
+        bassDrumOscillator.connect(bassDrumGain);
+        bassDrumGain.connect(context.destination);
+        bassDrumOscillator.start(now);
+    }
+    
+    var now = context.currentTime;
+    var duration = 3;
+    
+    bassDrumGain.gain.cancelScheduledValues(0);
+    // Reduce the gain to 0 to avoid a clicking noise when starting the bass drum sound again.
+    bassDrumGain.gain.setValueAtTime(0, now);
+    bassDrumGain.gain.setValueAtTime(1, now + 0.001);
+    bassDrumGain.gain.exponentialRampToValueAtTime(0.0001, now + duration);
+    bassDrumGain.gain.setValueAtTime(0, now + duration);
+    
+    bassDrumOscillator.frequency.setValueAtTime(150, now);
+    bassDrumOscillator.frequency.exponentialRampToValueAtTime(0.001, now + duration);
 }

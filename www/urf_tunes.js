e0484409260506444eaf207bfcab98eaf07cee73
red3141 <red3141@gmail.com>
2016-04-27 00:16:57 -0400

    Add a snare drum
    Values modified by a few AD assassins

--- www/urf_tunes.js
+++ www/urf_tunes.js
@@ -2,19 +2,23 @@ var context;
 
 var masteries = {
     'braum' : 5,
-    'hecarim' : 0,
+    'hecarim' : 5,
     'malphite' : 5,
-    'masteryi' : 0,
-    'rammus' : 0,
-    'zilean' : 0
+    'masteryi' : 5,
+    'rammus' : 5,
+    'rengar' : 1,
+    'talon' : 4,
+    'zed' : 5,
+    'zilean' : 5
 }
 
 const MAX_GAIN = 3;
+const BASICALLY_ZERO = 0.001; // Used when dropping gain to basically zero, since we can't exponentially drop to zero.
+
 const BEATS_PER_BAR = 4; // We're sticking with 4/4 time to start with.
-const BEATS_PER_MINUTE = 110 + 40 + 2 * (masteries['hecarim'] + masteries['masteryi'] + masteries['rammus'] + masteries['zilean']);
+const BEATS_PER_MINUTE = 110 + 1.5 * (masteries['hecarim'] + masteries['masteryi'] + masteries['rammus'] + masteries['zilean']);
 const SECONDS_PER_BEAT = 60.0 / BEATS_PER_MINUTE;
 const SECONDS_PER_BAR = BEATS_PER_BAR * SECONDS_PER_BEAT;
-console.log(SECONDS_PER_BEAT + "   " + SECONDS_PER_BAR);
 
 var songStartTime = 0;
 
@@ -69,18 +73,98 @@ BassDrum.prototype.play = function(bars, volume) {
     this.oscillator.frequency.setValueAtTime(this.pitch, time);
     this.gain.gain.setValueAtTime(MAX_GAIN * volume, time);
     
-    this.oscillator.frequency.exponentialRampToValueAtTime(0.001, endTime);
-    this.gain.gain.exponentialRampToValueAtTime(0.001, endTime);
+    this.oscillator.frequency.exponentialRampToValueAtTime(BASICALLY_ZERO, endTime);
+    this.gain.gain.exponentialRampToValueAtTime(BASICALLY_ZERO, endTime);
     
     this.oscillator.start(time);
     this.oscillator.stop(endTime);
 };
+
+// Snare Drum
+function SnareDrum(context, pitch, oscillatorDuration, noiseDuration, filterMinPitch) {
+    this.context = context;
+    this.noiseBuffer = this.createNoiseBuffer();
+    this.pitch = pitch;
+    this.oscillatorDuration = oscillatorDuration;
+    this.noiseDuration = noiseDuration;
+    this.filterMinPitch = filterMinPitch;
+}
+
+SnareDrum.prototype.createNoiseBuffer = function() {
+    var bufferSize = this.context.sampleRate;
+    var buffer = this.context.createBuffer(1, bufferSize, this.context.sampleRate);
+    var output = buffer.getChannelData(0);
+    for (var i = 0; i < bufferSize; ++i) {
+        output[i] = Math.random() * 2 - 1;
+    }
+    
+    return buffer;
+};
+
+SnareDrum.prototype.init = function() {
+    this.noise = this.context.createBufferSource();
+    this.noise.buffer = this.noiseBuffer;
+    var noiseFilter = this.context.createBiquadFilter();
+    noiseFilter.type = 'highpass';
+    noiseFilter.frequency.value = this.filterMinPitch;
+    this.noise.connect(noiseFilter);
+    this.noiseGain = this.context.createGain();
+    noiseFilter.connect(this.noiseGain);
+    
+    this.noiseGain.connect(this.context.destination);
+    
+    this.oscillator = this.context.createOscillator();
+    this.oscillator.type = 'triangle';
+    
+    this.oscillatorGain = this.context.createGain();
+    this.oscillator.connect(this.oscillatorGain);
+    this.oscillatorGain.connect(this.context.destination);
+};
+
+SnareDrum.prototype.setPitch = function(newPitch) {
+    this.pitch = newPitch;
+}
+
+SnareDrum.prototype.setOscillatorDuration = function(newOscillatorDuration) {
+    this.oscillatorDuration = newOscillatorDuration;
+}
+
+SnareDrum.prototype.setNoiseDuration = function(newNoiseDuration) {
+    this.noiseDuration = newNoiseDuration;
+}
+
+SnareDrum.prototype.setFilterMinPitch = function(newFilterMinPitch) {
+    this.filterMinPitch = filterMinPitch;
+}
+
+SnareDrum.prototype.play = function(bars) {
+    this.init();
+    
+    var time = songStartTime + SECONDS_PER_BAR * bars;
+    var oscillatorEndTime = time + this.oscillatorDuration;
+    var noiseEndTime = time + this.noiseDuration;
+    var endTime = Math.max(oscillatorEndTime, noiseEndTime);
+    
+    this.noiseGain.gain.setValueAtTime(MAX_GAIN * 0.3, time);
+    this.noiseGain.gain.exponentialRampToValueAtTime(BASICALLY_ZERO, noiseEndTime);
+    this.noise.start(time);
+    
+    this.oscillator.frequency.setValueAtTime(this.pitch, time);
+    this.oscillatorGain.gain.setValueAtTime(MAX_GAIN * 0.3, time);
+    this.oscillatorGain.gain.exponentialRampToValueAtTime(BASICALLY_ZERO, oscillatorEndTime);
+    this.oscillator.start(time);
+    
+    this.noise.stop(endTime);
+    this.oscillator.stop(endTime);
+}
 
 function playSong() {
     songStartTime = context.currentTime;
     
     // Create the instruments for the song.
     var bassDrum = new BassDrum(context, 150 + 20 * masteries['braum'], 0.1 + 0.1 * masteries['malphite']);
+    var snareDrum = new SnareDrum(context, 100, 0.1 + 0.03 * masteries['rengar'],
+            0.2 + 0.03 * masteries['talon'], 1500 - 200 * masteries['zed']);
     
     var delta = 0.25;
     var repeats = 4;
@@ -88,8 +172,12 @@ function playSong() {
     
     for (var i = 0; i < 4; ++i) {
         for (var j = 0; j < repeats; ++j) {
-            bassDrum.play(bars, 1);
+            snareDrum.play(bars);
+            if (j % 2 == 0) {
+                bassDrum.play(bars, 1);
+            }
             bars += delta;
+            if (j == 24) break;
         }
         delta /= 2;
         repeats *= 2;

dd4e20d06c4120df02485021a3f6a8fc7020f1f7
AJ Richardson <aj.ric.ar+git@gmail.com>
2016-04-27 20:05:39 -0400

    Added a markov chain for chord progressions

--- www/song_builder.js
+++ www/song_builder.js
@@ -3,21 +3,94 @@ var SongBuilder = (function() {
         // Start by building the form of the song (e.g. AABA)
         // A "rule" is a function that determines the probability of getting to each state from the current state.
         // It returns a stateMap, which is an array of probabilities for each state.
-        var formRule = function (state, index, length) {
+        var formRule = function (prevStates) {
             // state is a 0-based number
-            if (index == 0)
+            if (equals(prevStates, []))
                 return [1];
-            switch (state) {
-                case 0:
-                    // 40% chance of going to state 0, 50% to state 2, 10% to state 3
-                    return [.4, .5, .1];
-                case 1:
-                    return [.4, 0, .6];
-                default:
-                    return [.9, .1, 0];
-            }
+            if (equals(prevStates, [0]))
+                return [0.5, 0.5];
+            if (equals(prevStates, [0, 0]))
+                return [0, 1];
+            if (equals(prevStates, [0, 1]))
+                return [0.9, 0, 0.1];
+            if (equals(prevStates, [0, 0, 1]))
+                return [0.8, 0, 0.2];
+            if (equals(prevStates, [0, 1, 0]))
+                return [0, 0.9, 0.1];
+            if (equals(prevStates, [0, 1, 2]))
+                return [0.8, 0.2, 0];
+            if (prevStates.length === 6 && prevStates.indexOf(2) === -1)
+                return [0, 0, 1]; // Ensure that the C section happens at least once
+            if (prevStates.length < 2)
+                return [1];
+            var recentStates = prevStates.slice(prevStates.length - 2, prevStates.length);
+            if (equals(recentStates, [0, 0]))
+                return [0, 1];
+            if (equals(recentStates, [0, 1]))
+                return [0.3, 0, 0.7];
+            if (equals(recentStates, [0, 2]))
+                return [1];
+            if (equals(recentStates, [1, 0]))
+                return [0, 0.6, 0.4];
+            if (equals(recentStates, [1, 2]))
+                return [1];
+            if (equals(recentStates, [2, 0]))
+                return [0.1, 0.8, 0.1];
+            return [1];
         }
-        var form = new MarkovChain(formRule, 6);
+        var form = new MarkovChain(formRule, 8);
+        
+        // Now generate a chord progression
+        var chordRule = function (prevStates) {
+            // Data from http://www.hooktheory.com/trends#node=1&key=C
+            if (equals(prevStates, []))
+                return [1];
+            if (equals(prevStates, [0]))
+                return [0, 0.09, 0, 0.28, 0.48, 0.15, 0];
+            if (equals(prevStates, [0, 1]))
+                return [0.18, 0, 0.18, 0.20, 0.14, 0.30, 0];
+            if (equals(prevStates, [0, 1, 0]))
+                return [0, 0.54, 0, 0.18, 0.22, 0.06, 0];
+            if (equals(prevStates, [0, 1, 2]))
+                return [0, 0, 0, 0.8, 0, 0.2, 0];
+            if (equals(prevStates, [0, 1, 3]))
+                return [0, 0, 0, 0, 0.67, 0.33, 0];
+            if (equals(prevStates, [0, 1, 4]))
+                return [0.35, 0, 0, 0, 0.45, 0.2, 0];
+            if (equals(prevStates, [0, 1, 5]))
+                return [0.5, 0, 0, 0.2, 0, 0.3, 0];
+            if (equals(prevStates, [0, 3]))
+                return [0.4, 0, 0, 0, 0.4, 0.2, 0];
+            if (equals(prevStates, [0, 3, 0]))
+                return [0, 0, 0, 0.6, 0.4, 0, 0];
+            if (equals(prevStates, [0, 3, 4]))
+                return [0.4, 0, 0, 0.2, 0, 0.4, 0];
+            if (equals(prevStates, [0, 3, 5]))
+                return [0.1, 0, 0, 0.2, 0.7, 0, 0];
+            if (equals(prevStates, [0, 4]))
+                return [0.2, 0.1, 0, 0.3, 0, 0.4, 0];
+            if (equals(prevStates, [0, 4, 0]))
+                return [0, 0, 0, 0.5, 0.5, 0, 0];
+            if (equals(prevStates, [0, 4, 1]))
+                return [0, 0, 0, 0.7, 0, 0.3, 0];
+            if (equals(prevStates, [0, 4, 3]))
+                return [0.5, 0, 0, 0, 0.25, 0.25, 0];
+            if (equals(prevStates, [0, 4, 5]))
+                return [0, 0, 0, 0.8, 0.2, 0, 0];
+            if (equals(prevStates, [0, 5]))
+                return [0.15, 0, 0, 0.45, 0.35, 0, 0];
+            if (equals(prevStates, [0, 5, 0]))
+                return [0, 0, 0, 0.3, 0.1, 0.6, 0];
+            if (equals(prevStates, [0, 5, 3]))
+                return [0.5, 0, 0, 0, 0.5, 0, 0];
+            if (equals(prevStates, [0, 5, 4]))
+                return [0.2, 0.1, 0, 0.7, 0, 0, 0];
+        }
+        var chordProgressions = [];
+        var chordProgressionLength = 4;
+        for (var i = 0; i < 3; ++i) {
+            chordProgressions[i] = new MarkovChain(chordRule, chordProgressionLength);
+        }
         
         // Now generate a sequence of notes for each segment of song (0=A, 1=B, 2=C)
         var pitchRule = function (state, index, length) {
